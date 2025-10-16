93caa3a9e0495c57d7961297f98e23da15a80ccb
yahir <s22120159@alumnos.itsur.edu.mx>
2025-10-15 23:17:17 -0600

    Se implemento la funcionalidad al boton sell para reducir la cantidad en el stock

--- basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
+++ basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
@@ -105,7 +105,7 @@ fun ItemDetailsScreen(
     ) { innerPadding ->
         ItemDetailsBody(
             itemDetailsUiState  = uiState.value,
-            onSellItem = {  },
+            onSellItem = { viewModel.reduceQuantityByOne() },
             onDelete = { },
             modifier = modifier.padding(innerPadding)
                 .padding(
